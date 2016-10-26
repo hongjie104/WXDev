8af366790ea42c000e8c2f70088c4cef8bcf035f
hongjie <32968210@qq.com>
2016-10-26 22:33:01 +0800

    fixed a bug

--- wxdev.py
+++ wxdev.py
@@ -143,17 +143,7 @@ PAGE_WXML = '''<view class="container">
 </view>
 '''
 
-PAGE_WXSS = '''.container {
-	height: 100%;
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	justify-content: space-between;
-	padding: 200rpx 0;
-	box-sizing: border-box;
-}
-
-.textStyle {
+PAGE_WXSS = '''.textStyle {
 	display: flex;
 	flex-direction: column;
 	align-items: center;
